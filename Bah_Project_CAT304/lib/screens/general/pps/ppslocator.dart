edd1c8207c621eb5fd6fdf61701b41bc85f4e709
isyaew <133485374+isyaew@users.noreply.github.com>
2024-01-28 22:49:28 +0800

    Update ppslocator.dart

--- Bah_Project_CAT304/lib/screens/general/pps/ppslocator.dart
+++ Bah_Project_CAT304/lib/screens/general/pps/ppslocator.dart
@@ -81,7 +81,6 @@ class _PPSLocatorState extends State<PPSLocator> {
 
 // treat the listlocations as the PPS locations and show it as marker in the maps
   void showLocationData({required Map locationsData}) {
-    print('pukimak');
     locationsData.forEach((key, value) {
       if (key != 'key') {
         print(key);
